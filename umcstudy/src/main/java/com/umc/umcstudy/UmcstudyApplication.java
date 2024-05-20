cb335372f9e60aa515c024868bc171b11662a139
Jaehwan Hyun <jec0101@naver.com>
2024-05-20 13:24:51 +0900

    intial commit

---
+++ umcstudy/src/main/java/com/umc/umcstudy/UmcstudyApplication.java
@@ -0,0 +1,12 @@
+package com.umc.umcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcstudyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UmcstudyApplication.class, args);
+	}
+
+}
---
+++ umcstudy/src/test/java/com/umc/umcstudy/UmcstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.umcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
