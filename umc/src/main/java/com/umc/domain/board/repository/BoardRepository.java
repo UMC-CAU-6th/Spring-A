67f1af812e1bad61770e40a84c2dcc45e593837f
sijin1017 <trsj1017@naver.com>
2024-07-02 00:13:28 +0900

    게시글 조회 페이징 처리

--- umc/src/main/java/com/umc/domain/board/repository/BoardRepository.java
+++ umc/src/main/java/com/umc/domain/board/repository/BoardRepository.java
@@ -1,5 +1,4 @@
 package com.umc.domain.board.repository;
-
 import com.umc.domain.board.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,3 +10,4 @@ import java.awt.print.Pageable;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> findAll(Pageable pageable);
 }
+
